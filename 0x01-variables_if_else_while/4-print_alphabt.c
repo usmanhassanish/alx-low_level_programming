117135f1059f271c89cd9fbb7a2a296887e579ff
ussyneh <usmanhassanish@gmail.com>
2023-03-17 20:15:24 -0700

    hello

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Prints alphabet in lower case execpt for q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char e;
+	char q;
+	char small;
+
+	e = 'e';
+	q = 'q';
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		small = tolower(x);
+		if (small != e && small != q)
+		putchar(small);
+	}
+	printf("\n");
+	return (0);
+}
