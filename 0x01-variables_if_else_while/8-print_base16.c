4e23282cf0ab5910d41983df94a4df85e1656f2a
ussyneh <usmanhassanish@gmail.com>
2023-03-17 18:23:06 -0700

    hey

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*more headers goes there*/
+/*betty style do for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Description: Print negative and positive numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	for (j = 'a'; j < 'g'; j++)
+	{
+		putchar (j + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
