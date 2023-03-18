fed83a24a454aadaa5d78808037cf59458772966
ussyneh <usmanhassanish@gmail.com>
2023-03-17 17:17:28 -0700

    hey

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*more headers goes there*/
+/*betty style do for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Description: Print negative and positive numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	char small = tolower(x);
+		putchar(small);
+	}
+	printf("\n");
+	return (0);
+}
