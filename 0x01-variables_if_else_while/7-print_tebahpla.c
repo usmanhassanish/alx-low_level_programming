70c576a0edbb2eef29b8611321a3e8a2c236f406
ussyneh <usmanhassanish@gmail.com>
2023-03-17 19:16:49 -0700

    hey

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/*more headers goes there*/
+/*betty style do for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Description: Print negative and positive numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'Z'; x >= 'A'; x--)
+	{
+	char small = tolower(x);
+		putchar(small);
+	}
+	putchar('\n');
+	return (0);
+}
