abe2c768be95f47cf9d03a35a244e4247e93e902
ussyneh <usmanhassanish@gmail.com>
2023-03-17 20:40:22 -0700

    djjf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Prints out alphabet in lower and upper case
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int x;
+	int i;
+	char small;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		small = tolower(x);
+		putchar(small);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
