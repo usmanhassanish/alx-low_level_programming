fd00675e713d94ef469c795dc9dbeb19026ae79f
ussyneh <usmanhassanish@gmail.com>
2023-03-21 14:00:34 -0700

    hello

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,22 +2,22 @@
 /**
  * jack_bauer - Entry point
  *
- * Return: 0.
  */
 void jack_bauer(void)
 {
-        int min;
-        int hr;
-        for (hr = 0; hr < 24; hr++)
-        {
-                for (min = 0; min < 60; min++)
-                {
-                        _putchar((hr / 10) + '0');
-                        _putchar((hr % 10) + '0');
-                        _putchar(':');
-                        _putchar((min / 10) + '0');
-                        _putchar((min % 10) + '0');
-                        _putchar('\n');
-                }
-        }
+	int min;
+	int hr;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
