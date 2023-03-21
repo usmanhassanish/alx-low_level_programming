0491c863d55b588b3796f7ad8840836dac505d79
ussyneh <usmanhassanish@gmail.com>
2023-03-21 14:06:09 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,13 @@
  */
 int print_last_digit(int i)
 {
-        int num;
-        num = (i % 10);
-        if (i < 0)
-        {
-                num = (-1 * num);
-        }
-        _putchar(num + '0');
-        return (num);
+	int num;
+
+	num = (i % 10);
+	if (i < 0)
+	{
+		num = (-1 * num);
+	}
+	_putchar(num + '0');
+	return (num);
 }
