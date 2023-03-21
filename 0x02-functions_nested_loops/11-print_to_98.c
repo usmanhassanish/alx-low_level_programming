9d2497293f29fde94ae0ff5cad52e0476727b003
ussyneh <usmanhassanish@gmail.com>
2023-03-21 13:42:39 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,37 +2,35 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * print_to_98 - Entry point
  * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
+ * @n: n is the number inserted
  */
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while(n > 98)
+		while (n > 98)
 		{
-		printf("%d, ",n);
-		n--;
+			printf("%d, ", n);
+			n--;
 		}
 		printf("98");
 		printf("\n");
 	}
-	else if (n <98)
+	else if (n < 98)
 	{
-		while(n < 98)
-                {
-                printf("%d, ",n);
-                n++;
-                }
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 		printf("98");
 		printf("\n");
 	}
 	else
 	{
-		printf("%d",n);
+		printf("%d", n);
 		printf("\n");
 	}
 }
