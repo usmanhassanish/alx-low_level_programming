01aec982b09d8431b9ea6e4266b77db77720342b
ussyneh <usmanhassanish@gmail.com>
2023-03-21 12:42:49 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+	int sum;
+	sum = x + y;
+	return sum;
+}
+
