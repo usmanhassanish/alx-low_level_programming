b9ab26728311c65716636a09ebb991c38cd3a0c9
ussyneh <usmanhassanish@gmail.com>
2023-03-21 14:17:00 -0700

    hey

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,13 +3,13 @@
 /**
  * print_sign - Entry point
  *
- * Description: 'the program's description'
+ * @n: input from user
  *
- * Return: returns 0 is number is zero, -1 is number is negative, else the return is 1;
+ * Return: returns 0 is number is zero, -1 is number is negative
  */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
