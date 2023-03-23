0342ab2b66652d85fcd39791d9fe8c91a996d37a
ussyneh <usmanhassanish@gmail.com>
2023-03-23 14:21:49 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * positive_or_negative - Entry Point
- * @i: argument
- */
-void positive_or_negative(int i)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is either positiv or negative\n", i);
-	}
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
