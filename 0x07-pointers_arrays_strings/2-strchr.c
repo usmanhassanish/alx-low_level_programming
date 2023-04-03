6f1601cd2066a19bc3480eaf894c3a2eabe5f3df
ussyneh <usmanhassanish@gmail.com>
2023-04-03 04:49:29 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,19 @@
 #include "main.h"
-
+/**
+ * _strchr - copy a string
+ * @s: argument 1
+ * @c: argument 2
+ * Return: returns 0;
+ */
 char *_strchr(char *s, char c)
 {
-	while(*s++)
+	while (*s++)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
 	}
-	return 0;
+	return (0);
 
 }
