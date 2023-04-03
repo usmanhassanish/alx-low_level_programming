1128f7b6de2a8dae739bdee2fa9f125a6902cad3
ussyneh <usmanhassanish@gmail.com>
2023-04-03 10:17:06 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: argument
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k, b;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[k][b]);
+		}
+		_putchar('\n');
+	}
+}
