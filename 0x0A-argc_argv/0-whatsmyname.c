3f0d9c5fe7fbede3dd8b938bef6f1ddb0d991bb5
ussyneh <usmanhassanish@gmail.com>
2023-04-09 04:22:55 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,13 @@
  * @argv: an array of the names of paramaetrs
  * Return: returns zero
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
