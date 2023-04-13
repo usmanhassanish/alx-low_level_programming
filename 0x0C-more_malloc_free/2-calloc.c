4d8e217e3d0248cc20de21453a9659dbde56570b
ussyneh <usmanhassanish@gmail.com>
2023-04-13 07:31:00 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - memory allocation of an array
+ * @nmemb: number of elemenent in the array
+ * @size: size of array
+ * Return: ptr - ponter to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = malloc(size * nmemb);
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}	
+	if (ptr == NULL)
+        {
+                return (NULL);
+        }
+	return (ptr);
+}
