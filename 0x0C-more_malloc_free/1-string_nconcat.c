35eadbbf3adaa5d8c80d4193485e952286634427
ussyneh <usmanhassanish@gmail.com>
2023-04-13 05:01:35 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * string_nconcat - concantenate string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of characters from string 2
+ * Return: returns pointer to string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
 	unsigned int k = 0;
 	unsigned int j = 0;
 	char *ptr;
-	while(s1[k] != '\0')
+
+	while (s1[k] != '\0')
 	{
-        	k++;
+		k++;
 	}
 	ptr = malloc((sizeof(char) * k) + n);
-
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < (k + n); i++)
-        {
+	{
 		if (i >= k)
 		{
 			ptr[i] = s2[j];
@@ -24,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			ptr[i] = s1[i];
 		}
 
-        }
-        ptr[k + n] = '\0';
-        return (ptr);
+	}
+	ptr[k + n] = '\0';
+	return (ptr);
 }
