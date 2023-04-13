151743ce259612d2bcc2c59b218a9a83b7a4e293
ussyneh <usmanhassanish@gmail.com>
2023-04-13 15:05:30 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocate memory
+ * @ptr: argument 1
+ * @old_size: argument 2
+ * @new_size: argument 3
+ * Return: return null or ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+	{
+		ptr = malloc(old_size);
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr = malloc(new_size);
+	return (ptr);
+}
