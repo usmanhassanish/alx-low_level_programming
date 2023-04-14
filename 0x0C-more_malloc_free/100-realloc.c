6d5cc7965e505a5daa54e4d319b12878023352d7
ussyneh <usmanhassanish@gmail.com>
2023-04-13 18:30:06 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	ptr = malloc(new_size);
+	ptr = malloc(new_size + 1);
 	return (ptr);
 
 }
