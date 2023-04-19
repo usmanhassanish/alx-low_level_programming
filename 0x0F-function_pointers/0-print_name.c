1e313cde4c548cd95cbdaa5389e51112c6a084b4
ussyneh <usmanhassanish@gmail.com>
2023-04-19 07:39:35 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f:pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _functions_
+#define _functions_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
