e8002753438cf99e43e541eb6fc10245967c04bf
ussyneh <usmanhassanish@gmail.com>
2023-06-15 04:39:31 -0700

    updated

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,21 @@
 #include <stddef.h>
 #include "lists.h"
 #include <stdio.h>
+/**
+ * dlistint_len - length of elements in a list
+ * @h: argument 1
+ * Return: returns number of elements in list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-        const dlistint_t *current = h;
-        size_t count = 0;
+	size_t count = 0;
+	const dlistint_t *current = h;
 
-        while (current != NULL)
-        {
-                current = current->next;
-                count++;
-        }
-        return (count);
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
 }
