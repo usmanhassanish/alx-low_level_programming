87c7f2d07f1bb3848e8e122da3908ebe8430029e
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2023-07-07 10:57:44 +0100

    update task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - Hash function to implement djb2
+ * @str: string argument
+ * Return: returns the hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int i;
+
+	hash = 5381;
+	while ((i = *str++))
+		hash = ((hash << 5) + hash) + i; 
+	return (hash);
+}
