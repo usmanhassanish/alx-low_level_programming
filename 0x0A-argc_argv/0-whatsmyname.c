d6dab8f8c1eb7384cd00c7852ca23610f57438c7
ussy <usmanhassanish@gmail.com>
2023-10-27 02:11:37 -0700

    updated task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counts number of arguments in main
+ * @argv: array that holds arguments in main
+ * Return: returns 0 on sucess
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
