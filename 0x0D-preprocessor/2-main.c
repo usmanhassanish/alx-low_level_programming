2d1cecc27e87455f08772912129153d5b20e24d6
ussy <usmanhassanish@gmail.com>
2023-11-08 06:20:46 -0800

    task 3 completed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc: number count of arguments
+ * @argv: array holding arguments to the main
+ * Return: returns 0
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _ABS
+#define _ABS
+#define ABS(x) ((x) < 0? -(x) : (x))
+#endif
