c989a94ffcf4b72319f85778fa841e6303a79047
ussy <usmanhassanish@gmail.com>
2023-11-08 07:52:00 -0800

    task 0 completed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - prints a name
  * @name: name argument
@@ -6,5 +7,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
+	if (name == NULL)
+		return;
 	(*f)(name);
 }
