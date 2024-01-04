d352c1002239f8390816e3f5ec350358c4de4a1f
ussy <usmanhassanish@gmail.com>
2024-01-04 06:25:07 -0800

    updated task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to upper
+ * @s: letter to be changed
+ * Return: returns letters
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
 #endif
