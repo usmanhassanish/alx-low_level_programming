804cc47151daebe13c057112688e79c944809edd
ussy <usmanhassanish@gmail.com>
2024-01-09 06:00:03 -0800

    task 1 advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets a value to another string
+ * @s: string to be changed
+ * @to: string to replace
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
