59f84bc0e70e73d2999e7c9923e64a6d1fbefd06
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-11 14:22:59 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * _puts_recursion - prints strings recursively
- * @s: string to be printed
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
