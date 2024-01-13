00acfd3aca04bb45d6d09c3158bd7d864bcd8a11
ussy <usmanhassanish@gmail.com>
2024-01-13 07:01:00 -0800

    task 1 and 2 completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - the entry point of the program
+ * @argc: argument count
+ * @argv: array of argumensts
+ * Return: returns 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ * @argc: arguments count
+ * @argv: argument vector count
+ * Return: returns o
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
