a2a2561df39e0db7c26bd088c4d5299838976286
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-20 12:04:01 +0100

    updated task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+/**
+ * factorial - computes factorial of a number
+ * @n: number
+ * Return: returns factoial or -1
+ */
+int factorial(int n)
+{
+  if (n < 0)
+    return (-1);
+  if (n == 0)
+    return (0);
+  return (n) * factorial(n - 1);
+}
