79a98ea3ea9d93e76d14410136cb7c3f715b0217
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-26 15:07:51 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,8 +27,14 @@ void print_all(const char * const format, ...)
             (i == (argumentCount - 1)) ? printf("%f\n", va_arg(ap, double)) : printf("%f, ", va_arg(ap, double));
             break;
         case 's':
-            (i == (argumentCount - 1)) ? printf("%s\n", va_arg(ap, char *)) : printf("%s, ", va_arg(ap, char *));
-            break;
+            {
+                char *str = va_arg(ap, char *);
+                if (str == NULL) {
+                    (i == (argumentCount - 1)) ? printf("(nil)\n") : printf("(nil), ");
+                } else {
+                    (i == (argumentCount - 1)) ? printf("%s\n", str) : printf("%s, ", str);
+                }
+            }
         default:
             break;
     }
