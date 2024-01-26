a1a17064ddb1edc25693cd057969bb7f02446444
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-26 13:34:56 +0100

    Create 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+
+void print_all(const char * const format, ...)
+{
+    int argumentCount = 0, i = 0;
+    va_list ap;
+    
+    while(format[argumentCount] != '\0')
+    {
+        argumentCount++;
+    }
+    va_start(ap, format);
+    while(i < argumentCount)
+    {
+        if (format[i] == 'c')
+            (i == (argumentCount - 1))?printf("%c\n", va_arg(ap, int)):printf("%c, ", va_arg(ap, int));
+        else if (format[i] == 'i')
+            (i == (argumentCount - 1))?printf("%i\n", va_arg(ap, int)):printf("%i, ", va_arg(ap, int));
+        else if (format[i] == 'f')
+            (i == (argumentCount - 1))?printf("%f\n", va_arg(ap, float)):printf("%f, ", va_arg(ap, float));
+        else if (format[i] == 's')
+            (i == (argumentCount - 1))?printf("%s\n", va_arg(ap, char *)):printf("%s, ", va_arg(ap, char *));
+        else
+        {
+            
+        }
+        i++;
+    }
+    va_end(ap);
+}
