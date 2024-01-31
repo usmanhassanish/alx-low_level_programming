b6a62d743ee81745cf8675dfcf155e510ec8b05d
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-31 13:05:08 +0100

    task 1 completed

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a new dog
+ * @d: dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        struct dog d;
+
+        d.name = name;
+        d.age = age;
+        d.owner = owner;
+}
