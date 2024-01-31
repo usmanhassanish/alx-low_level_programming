ee9ce2a69ce043a1edc3ff4686f964baffce5939
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-31 13:37:46 +0100

    task 2 updated

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+/**
+ * print_dog - prints the elements of dog
+ * @d: dog structure
+ */
+void print_dog(struct dog *d)
+{
+  if (d == NULL)
+  {
+    
+  }
+  else
+  {
+    if (d.name = NULL)
+      printf("Name: (nil)")
+    if (d.age = NULL)
+      printf("Age: (nil)")
+    if (d.owmer = NULL)
+      printf("Owner: (nil)")
+    
+    printf("%s", d.name);
+    printf("%f", d.age);
+    printf("%s", d.owner);
+  }
+}
