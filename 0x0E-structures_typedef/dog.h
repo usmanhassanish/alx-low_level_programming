36e17e06bd5ae67736730359cda1b5317d5d9950
ussy <usmanhassanish@gmail.com>
2024-01-31 03:48:23 -0800

    task 0 fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _dog_
 #define _dog_
+/**
+ * struct dog - defines a struct named dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
